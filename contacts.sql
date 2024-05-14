efaf64d5d2212dc0896713aabc61d3b3e450c27e
unknown <manuela.granzotto.lec_a@msft.cesae.pt>
2024-05-14 15:59:18 +0100

    add contacts table

--- contacts.sql
+++ contacts.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS contacts_mg
+DROP TABLE IF EXISTS contacts
 
-CREATE TABLE contacts_mg (
+CREATE TABLE contacts (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(384) NOT NULL UNIQUE
 );
 
-INSERT INTO contacts_mg (first_name, last_name, email) 
+INSERT INTO contacts (first_name, last_name, email) 
 VALUES
     ('John', 'Doe', 'john.doe@example.com'),
     ('Jane', 'Smith', 'jane.smith@example.com'),
